3db5f582070c6bc752a3ee7466de112f11616db3
Lee_Margolies <lfmargolies@gmail.com>
2014-05-13 19:26:50 -0400

    labs 2 and 3

---
+++ src/edu/gmu/bah/class14/lab2/Vehicle2.java
@@ -0,0 +1,11 @@
+package edu.gmu.bah.class14.lab2;
+
+/**
+ * Created by student on 5/13/14.
+ */
+public interface Vehicle2 {
+    void drive(int speed);
+    int wheel_count();
+    int door_count();
+    String honk();
+}
